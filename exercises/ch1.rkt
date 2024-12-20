8b86a3d8049ca0baa5ae5339d53a196ca0d06314
Ziqin Gong <gongzq0301@gmail.com>
2024-12-20 20:08:11 +0800

    exer 1.7 1.8

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -24,3 +24,27 @@
            (sum-of-squares a c))
           (else
            (sum-of-squares a b)))))
+
+(define (exercise-1-7 x)
+  (define (sqrt-iter last-guess guess x)
+    (if (good-enough? last-guess guess)
+        guess
+        (sqrt-iter guess (improve guess x) x)))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (good-enough? last-guess guess)
+    (< (abs (- last-guess guess)) 0.001))
+  (define (average a b)
+    (/ (+ a b) 2))
+  (sqrt-iter 0.0 1.0 x))
+
+(define (exercise-1-8 x)
+  (define (cube-root-iter last-guess guess x)
+    (if (good-enough? last-guess guess)
+        guess
+        (cube-root-iter guess (improve guess x) x)))
+  (define (improve guess x)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+  (define (good-enough? last-guess guess)
+    (< (abs (- last-guess guess)) 0.001))
+  (cube-root-iter 0.0 1.0 x))
