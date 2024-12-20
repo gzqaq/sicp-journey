6e04c3df2c526e961d4bc8d60eddc865c67f3147
Ziqin Gong <gongzq0301@gmail.com>
2024-12-20 16:21:12 +0800

    conditional expressions and predicates

--- sicp-eg.rkt
+++ sicp-eg.rkt
@@ -9,3 +9,25 @@
   (+ (square x) (square y)))
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
+
+;; case analysis using cond(itional)
+(define (abs--cond-1 x)
+  (cond ((> x 0) x)  ; clause = (predicate expression)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(define (abs--cond-2 x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(define (abs--if x)
+  (if (< x 0)
+      (- x)
+      x))
+
+;; greater than or equal to using logical composition operations
+(define (>=--or x y)
+  (or (> x y) (= x y)))  ; or, and are special forms
+
+(define (>=--not x y)
+  (not (< x y)))  ; not is an ordinary procedure
