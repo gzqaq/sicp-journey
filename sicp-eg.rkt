7b857a53faa2204f197924beb4eb8b852dfca6a5
Ziqin Gong <gongzq0301@gmail.com>
2024-12-20 16:19:15 +0800

    compound procedures

--- sicp-eg.rkt
+++ sicp-eg.rkt
@@ -2,3 +2,10 @@
 
 ;; name things with define
 (define pi 3.14159)
+
+;; procedure definition
+(define (square x) (* x x))
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
