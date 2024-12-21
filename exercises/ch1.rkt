1c616ca0e988df631d8bff878abf58c07e9db57e
Ziqin Gong <gongzq0301@gmail.com>
2024-12-21 15:06:16 +0800

    exer 1.11 1.12

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -48,3 +48,29 @@
   (define (good-enough? last-guess guess)
     (< (abs (- last-guess guess)) 0.001))
   (cube-root-iter 0.0 1.0 x))
+
+(define (exercise-1-11-rec n)
+  (if (< n 3)
+      n
+      (+ (exercise-1-11-rec (- n 1))
+         (* 2 (exercise-1-11-rec (- n 2)))
+         (* 3 (exercise-1-11-rec (- n 3))))))
+
+(define (exercise-1-11-iter n)
+  (define (f--iter a b c counter)
+    (if (= counter 0)
+        c
+        (f--iter (+ a
+                    (* 2 b)
+                    (* 3 c))
+                 a
+                 b
+                 (- counter 1))))
+  (f--iter 2 1 0 n))
+
+(define (exercise-1-12 a b)
+  (if (or (= a b)
+          (= 1 b))
+      1
+      (+ (exercise-1-12 (- a 1) (- b 1))
+         (exercise-1-12 (- a 1) b))))
