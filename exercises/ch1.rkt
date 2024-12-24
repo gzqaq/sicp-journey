5e343e3cbfcb444087dae573d71fcca4b7491d90
Ziqin Gong <gongzq0301@gmail.com>
2024-12-24 19:36:54 +0800

    exer 1.23

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -145,9 +145,13 @@
   (define (find-divisor n test-divisor)
     (cond ((> (square test-divisor) n) n)
           ((divides? test-divisor n) test-divisor)
-          (else (find-divisor n (+ test-divisor 1)))))
+          (else (find-divisor n (next test-divisor)))))
   (define (divides? a b)
     (= (remainder b a) 0))
+  ;; (define (next n)
+  ;;   (+ n 1))
+  (define (next n)
+    (if (= n 2) 3 (+ n 2)))
   (find-divisor n 2))
 
 ; probabilistic prime? by Fermat test
