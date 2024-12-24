f4aa6babc139d75ef67b718e31052afa7b447674
Ziqin Gong <gongzq0301@gmail.com>
2024-12-24 18:11:26 +0800

    exer 1.22

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -115,3 +115,53 @@
                           p q
                           (- counter 1)))))
   (fib-iter 1 0 0 1 n))
+
+;; exercise 1.22
+(define (timed-prime-test n)
+  (define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))))
+  (define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (search-for-primes beg)
+  (define (search-from-odd n)
+    (if (prime? n)
+        n
+        (search-from-odd (+ n 2))))
+  (search-from-odd (if (even? beg) (+ 1 beg) beg)))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+;; (define (prime? n)
+;;   (fast-prime? n 11))
+
+; prime? by smallest-divisor
+(define (smallest-divisor n)
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (find-divisor n 2))
+
+; probabilistic prime? by Fermat test
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
