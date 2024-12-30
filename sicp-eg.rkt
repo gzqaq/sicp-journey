53316533e0f3cef59f155229845eb37229be48bb
Ziqin Gong <gongzq0301@gmail.com>
2024-12-31 06:39:50 +0800

    procedures as arguments

--- sicp-eg.rkt
+++ sicp-eg.rkt
@@ -174,3 +174,31 @@
   (cond ((= times 0) true)
         ((fermat-test n) (fast-prime? n (- times 1)))
         (else false)))
+
+;; a higher-order procedure, sum
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+;; various sum
+(define (cube n) (* n n n))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+; pi = (* 8 (pi-sum 1 1000000...))
+(define (pi-sum a b)
+  (define (pi-term x) (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x) (+ x 4))
+  (sum pi-term a pi-next b))
+
+;; integral by sum
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
