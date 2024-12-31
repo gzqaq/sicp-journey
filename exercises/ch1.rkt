173e9128423a475c7fe9e2ca504f6020b0660964
Ziqin Gong <gongzq0301@gmail.com>
2024-12-31 13:12:09 +0800

    exer 1.29

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -169,3 +169,21 @@
   (cond ((= times 0) true)
         ((fermat-test n) (fast-prime? n (- times 1)))
         (else false)))
+
+;; exercise 1.29
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral-simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (term k)
+    (* (cond ((= k 0) 1)
+             ((= k n) 1)
+             ((even? k) 2)
+             (else 4))
+       (f (+ a (* k h)))))
+  (* (/ h 3)
+     (sum term 0 inc n)))
