0d178c52a1268ca8e8af9904aeb4f8885f064c49
Ziqin Gong <gongzq0301@gmail.com>
2025-02-14 14:52:16 +0800

    exer 1.44

--- exercises/ch1.rkt
+++ exercises/ch1.rkt
@@ -302,3 +302,16 @@
         res
         (repeat-iter (compose f res) (- i 1))))
   (repeat-iter f (- n 1)))
+
+;; exercise 1.44
+(define dx 0.000001)
+
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx)))
+       3)))
+
+(define (n-fold-smooth f n)
+  ((repeated smooth n) f))
