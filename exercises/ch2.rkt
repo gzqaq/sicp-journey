e0819b52afc0e515f9457adc689ac76823f3d4c8
Ziqin Gong <gongzq0301@gmail.com>
2025-03-21 16:25:10 +0800

    exer 2.2

--- exercises/ch2.rkt
+++ exercises/ch2.rkt
@@ -6,3 +6,21 @@
         (denom-sign (if (> d 0) 1 (- 1))))
     (cons (* (/ n g) denom-sign)
           (* (/ d g) denom-sign))))
+
+
+;; exer 2.2
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment x y) (cons x y))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
