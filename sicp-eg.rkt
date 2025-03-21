4dcbf384413c7c5dfe6bba407437c0d4dfef397e
Ziqin Gong <gongzq0301@gmail.com>
2025-03-21 15:29:20 +0800

    arithmetic operations for rational numbers

--- sicp-eg.rkt
+++ sicp-eg.rkt
@@ -1,5 +1,9 @@
 #lang sicp
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Chapter 1: Building Abstractions with Procedures ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; name things with define
 (define pi 3.14159)
 
@@ -322,3 +326,49 @@
    (lambda (y) (- (square y) x))
    newton-transform
    1.0))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Chapter 2: Building Abstractions with Data ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; assuming we have the constructor `make-rat' and selectors `numer' and `denom'
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+;; The single compound-data primitive "pair", implemented by `cons', `car', and `cdr', is the only
+;; glue we need to construct data objects, which will be called "list-structred" data.
+(define (make-rat--raw n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+;; pretty print
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+;; use `gcd' to reduce the numerator and denominator to lowest terms before constructing the pair
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
