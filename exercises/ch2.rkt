a3e37f3221d5b6b767a50ffc48a026c8aa8b8652
Ziqin Gong <gongzq0301@gmail.com>
2025-04-18 16:02:43 +0800

    exer 2.6

--- exercises/ch2.rkt
+++ exercises/ch2.rkt
@@ -27,26 +27,34 @@
 
 
 ;; exer 2.4
-(define (exer-2-4-cons x y)
+(define (2-4-cons x y)
   (lambda (m) (m x y)))
-(define (exer-2-4-car z)
+(define (2-4-car z)
   (z (lambda (p q) p)))
-(define (exer-2-4-cdr z)
+(define (2-4-cdr z)
   (z (lambda (p q) q)))
 
 
 ;; exer 2.5
-(define (exer-2-5-cons a b)
+(define (2-5-cons a b)
   (* (expt 2 a) (expt 3 b)))
-(define (exer-2-5-car z)
+(define (2-5-car z)
   (define (car-iter n cnt)
     (if (= (remainder n 2) 0)
         (car-iter (/ n 2) (+ cnt 1))
         cnt))
   (car-iter z 0))
-(define (exer-2-5-cdr z)
+(define (2-5-cdr z)
   (define (cdr-iter n cnt)
     (if (= (remainder n 3) 0)
         (cdr-iter (/ n 3) (+ cnt 1))
         cnt))
   (cdr-iter z 0))
+
+
+;; exer 2.6
+(define 2-6-zero (lambda (f) (lambda (x) x)))
+(define (2-6-add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+(define 2-6-one (lambda (f) (lambda (x) (f x))))
+(define 2-6-two (lambda (f) (lambda (x) (f (f x)))))
